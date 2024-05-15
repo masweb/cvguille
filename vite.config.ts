52949eb4a19113b3b5df9bb5186e9142f4e57473
Guillermo Valentín <masweb@me.com>
2024-05-15 12:24:00 +0200

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cvguille/',
   plugins: [
     vue(),
   ],
@@ -18,6 +19,7 @@ export default defineConfig({
     hmr: {
       port: 3332,
     },
-  }
+  },
+
 
 })
